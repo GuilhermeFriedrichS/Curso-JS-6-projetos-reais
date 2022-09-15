b897d08bcf0321af358ca7eaf14f3e2259c52047
Guilherme Friedrich Sousa <guilhermefriedrichsousa@gmail.com>
2022-09-15 09:04:21 -0300

    C05 - Data e Hora, created clock with date on calculator

--- projeto-calculadora-curso-JS/scripts/calculator.js
+++ projeto-calculadora-curso-JS/scripts/calculator.js
@@ -1,1 +1,1 @@
-let calculator = new CalcControler;
+window.calculator = new CalcControler; //Declarando com .window para ser uma variavel global, pode ser acessada pelo console
--- projeto-calculadora-curso-JS/scripts/controller/CalcController.js
+++ projeto-calculadora-curso-JS/scripts/controller/CalcController.js
@@ -2,21 +2,66 @@ class CalcControler{
 
     constructor(){
 
-        this._displayCalc = "0";
+        this.locale = 'pt-BR'
         this._currentDate; //O _ faz atributos privados
-        this.inicialize();
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this.initialize();
     }
 
-    inicialize(){
+    initialize(){
+        
+        this.setdisplayDateTime();
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        setInterval(() => {
 
-        displayCalcEl.innerHTML = "4567"; 
-        dateEl.innerHTML = "14/09/2022";
-        timeEl.innerHTML = "00:00";
+            this.setdisplayDateTime();
+
+        },1000); //execulta a função dentro deste intervalo
     }
+
+    setdisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayCalc(){ // consulta o valor do atributo privado
+        return this._displayCalcEl.innerHTML
+    }
+    set displayCalc(value){ // atribui o valor do atributo privado, sempre que criar um atributo privado é preciso criar get e set
+        this._displayCalcEl.innerHTML = value;
+    }
+
     
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(valor){
+        this._dateEl.innerHTML = valor;
+    }
+
+   get currentDate(){
+        return new Date();
+   }
+   set currentDate(value){
+        this._currentDate = value;
+   }
+
+   
 }
 
