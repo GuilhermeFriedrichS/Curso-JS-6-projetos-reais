27b08e85848c6cc34bb8a3af39d0d9834686a18c
Guilherme Friedrich Sousa <guilhermefriedrichsousa@gmail.com>
2022-09-19 12:42:07 -0300

    G13 - Trabalhando com arquivos - File Reader, Created getPhoto method that get and display photo

--- projeto-usuarios-curso-JS/controllers/UserController.js
+++ projeto-usuarios-curso-JS/controllers/UserController.js
@@ -14,9 +14,44 @@ class UserController {
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault(); //cancela a atualização da pagina do metodo post do form
-    
-            this.addLine (this.getValues());
+            
+            let values = this.getValues();
+
+        
+            this.getPhoto((content)=>{
+
+                values.photo = content;
+                
+                this.addLine(values);
+
+            });
+
+            
         })
+
+    }
+
+    getPhoto(callback){
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item =>{
+
+            if(item.name === "photo"){
+                return item
+            }
+            
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = ()=>{ //Quando acabar de carregar a imagem execulta a função
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
 
     }
 
@@ -56,7 +91,7 @@ class UserController {
 
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
