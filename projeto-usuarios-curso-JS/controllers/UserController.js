5a8c57de8898162f98c8f6a777df4df03fb28e50
Guilherme Friedrich Sousa <guilhermefriedrichsousa@gmail.com>
2022-09-21 10:24:05 -0300

    G29 - Refactoring - Otimizando o Template, Refactoring code and fix edit button

--- projeto-usuarios-curso-JS/controllers/UserController.js
+++ projeto-usuarios-curso-JS/controllers/UserController.js
@@ -47,20 +47,11 @@ class UserController {
                         result._photo = content;
                     }
 
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                        <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${(result._admin) ? "Sim" : "Não"}</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    `
-                    this.addEventsTr(tr);
+                    user.loadFromJSON(result);
+
+                    this.getTr(user, tr);
 
                     this.updateCount();
 
@@ -245,13 +236,22 @@ class UserController {
         users.push(data);
 
         //sessionStorage.setItem("users", JSON.stringify(users)); Para adicionar informações que perde com F5
-        localStorage.setItem("users", JSON.stringify(users)); // Para informações que só perdem limpando navegador
+        localStorage.setItem("users", JSON.stringify(users)); // Para informações que só perde limpando o navegador
 
     }
 
     addLine(dataUser){
 
-        let tr = document.createElement('tr');
+        let tr = this.getTr(dataUser);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    getTr(dataUser, tr = null){//tr valor opcional
+
+        if (tr === null) tr = document.createElement('tr');
 
         tr.dataset.user = JSON.stringify(dataUser); //Transforma o objeto em uma string JSON para ser interpretada como objeto novamente depois
 
@@ -265,12 +265,11 @@ class UserController {
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
-        `
+        `;
+
         this.addEventsTr(tr);
 
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
+        return tr;
     }
 
     addEventsTr(tr){
