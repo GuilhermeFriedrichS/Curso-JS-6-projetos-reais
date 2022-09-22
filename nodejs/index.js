9d781b4cbf84e224044cfcb163ad0fff445fc186
Guilherme Friedrich Sousa <guilhermefriedrichsousa@gmail.com>
2022-09-22 09:22:54 -0300

    N06 - Nodemon e Criando o Servidor com Express, Changed code to work with Express

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,38 +1,29 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) =>{
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('URL:', req.method);
+app.get('/', (req, res) =>{
 
-    switch(req.url){
-
-        case '/':
-        
-        res.statusCode = 200; //codigo 200 é o codigo que devemos retornar para o usuario quando a conexão dá certo
-        res.setHeader('Content-Type', 'text/html'); // Faz ele ler o res.end como html
-        res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/users':
-
-        res.statusCode = 200; 
-        res.setHeader('Content-Type', 'application/json'); 
-        res.end(JSON.stringify({
-            users:[{
-                name:'Guilherme',
-                email:'teste@teste',
-                id:1
-            }]
-        }));
-
-        break;
-
-    }
+    res.statusCode = 200; //codigo 200 é o codigo que devemos retornar para o usuario quando a conexão dá certo
+    res.setHeader('Content-Type', 'text/html'); // Faz ele ler o res.end como html
+    res.end('<h1>Olá</h1>');
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.get('/users', (req, res) =>{
+
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'application/json'); 
+    res.end(JSON.stringify({
+        users:[{
+            name:'Guilherme',
+            email:'teste@teste',
+            id:1
+        }]
+    }));
+
+});
+
+app.listen(3000, '127.0.0.1', ()=>{
 
     console.log('Servidor rodando!')
 
