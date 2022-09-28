68815fd6fdbe2329078e740a93d995f7608da3cf
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-09-28 09:55:32 -0300

    D12 - Selecionando arquivos com CTRL ou SHIFT, Created method to display selected files

--- projeto-dropbox-curso-JS/app/public/src/controller/DropBoxController.js
+++ projeto-dropbox-curso-JS/app/public/src/controller/DropBoxController.js
@@ -183,7 +183,6 @@ class DropBoxController {
     }
 
     getFileIconView(file) {
-      console.log(file);
         switch (file.mimetype) {
           case 'folder':
             return `<svg width="160" height="160" viewBox="0 0 160 160" class="mc-icon-template-content tile__preview tile__preview--icon">
@@ -350,7 +349,7 @@ class DropBoxController {
         ${this.getFileIconView(file)}
         <div class="name text-center">${file.originalFilename}</div>
       ` 
-  
+      this.initEventsLi(li);
       return li;
     }
 
@@ -367,9 +366,63 @@ class DropBoxController {
 
           this.listFilesEl.appendChild(this.getFileView(data, key))
 
-        })
+        });
 
-      })
+      });
+
+    }
+
+    initEventsLi(li){
+
+      li.addEventListener('click', e => {
+
+        if (e.shiftKey){
+
+          let firstLi = this.listFilesEl.querySelector('.selected');
+          if(firstLi){
+
+            let indexStart;
+            let indexEnd;
+            let lis = li.parentElement.childNodes;
+
+            lis.forEach((el, index) => {
+
+              if(firstLi === el) indexStart = index;
+              if(li === el) indexEnd = index;
+
+            });
+
+            let index = [indexStart, indexEnd].sort();
+
+            lis.forEach((el, i) => {
+
+              if (i >= index [0] && i <= index [1]){
+
+                el.classList.add('selected');
+
+              }
+
+            });
+            
+            return true;
+
+          }
+
+        }
+
+        if(!e.ctrlKey){
+
+          this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+
+            el.classList.remove('selected');
+
+          });
+
+        }
+
+        li.classList.toggle('selected');
+
+      });
 
     }
 }
