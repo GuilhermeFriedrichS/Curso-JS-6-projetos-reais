2942a9ac34e650d8db1a8998837fd3164438542f
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-09-29 10:14:17 -0300

    D19 - Criando rota para abrir arquivo, Created route to open files

--- projeto-dropbox-curso-JS/app/routes/index.js
+++ projeto-dropbox-curso-JS/app/routes/index.js
@@ -8,6 +8,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/file', (req, res) => {
+
+  let path = '/' + req.query.path;
+  console.log(req.query.path);
+
+  
+  if(fs.existsSync(path)){
+
+    fs.readFile(path, (err, data)=>{
+
+      if(err){
+
+        console.error(err);
+        res.status(404).json({
+          error: err
+        }); 
+
+      }else{
+
+        res.status(200).end(data);
+
+      }
+
+    });
+  
+  }else{
+
+    res.status(404).json({
+      error:'File not found'
+    });
+
+  }
+
+});
+
 router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
@@ -38,7 +74,11 @@ router.delete('/file', (req, res) => {
 
       });
 
-    }
+    }else{
+      res.status(404).json({
+      error:'File not found'
+    })}
+    
   })
 })
 
