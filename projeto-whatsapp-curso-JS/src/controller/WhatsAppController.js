47cc141a2eba18c67c266f169b083b489d8694a6
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-03 11:32:54 -0300

    W10 - Eventos do campo Digitar Mensagem, Created events to hide placeholder massage and get emojis unicode

--- projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
+++ projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
@@ -34,7 +34,7 @@ class WhatsAppController{
             return this;
 
         }
-        Element.prototype.toogle = function(){
+        Element.prototype.toggle = function(){
 
             this.style.display = (this.style.display === 'none') ? 'block' : 'none'; 
             return this;
@@ -69,11 +69,12 @@ class WhatsAppController{
             this.classList.remove(name);
             return this;
         } 
-        Element.prototype.toogleClass = function (name){
+        Element.prototype.toggleClass = function (name) {
 
-            this.classList.toogle(name);
+            this.classList.toggle(name);
             return this;
-        } 
+
+        }
         Element.prototype.hasClass = function (name){
 
             return this.classList.contais(name);
@@ -264,6 +265,56 @@ class WhatsAppController{
         this.el.btnFinishMicrophone.on('click', e=>{
 
             this.CloseRecordMicrophone();
+
+        });
+
+        this.el.inputText.on('keypress', e=>{
+
+            if(e.key === 'Enter' && !e.ctrlKey){
+                
+                e.preventDefault();
+                this.el.btnSend.click();
+
+            }
+
+        });
+
+        this.el.inputText.on('keyup', e=>{
+            
+            if(this.el.inputText.innerHTML.length){
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+
+            }else{
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+
+            }
+
+        });
+
+        this.el.btnEmojis.on('click', e=>{
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji=>{
+
+            emoji.on('click', e=>{
+
+                console.log(emoji.dataset.unicode);
+
+            })
+
+        });
+
+        this.el.btnSend.on('click', e=>{
+
+            console.log(this.el.inputText.innerHTML);
 
         });
     }
