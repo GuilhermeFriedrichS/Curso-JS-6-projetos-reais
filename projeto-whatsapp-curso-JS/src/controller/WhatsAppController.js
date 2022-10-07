29f27ecd1c254845af84a3a21790ee0a9c2cf082
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-07 09:26:09 -0300

    W34 - Class Chat - Criando uma conversa e usando função btoa() conversão base64, Created class Chat to create chat between two users

--- projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
+++ projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
@@ -4,6 +4,8 @@ import {MicrophoneController} from './MicrophoneController';
 import {DocumentPreviewController} from './DocumentPreviewController';
 import {Firebase} from './../util/Firebase';
 import { User } from '../model/User';
+import { Chat } from '../model/Chat';
+
 
 export class WhatsAppController{
 
@@ -31,7 +33,7 @@ export class WhatsAppController{
                 document.querySelector('title').innerHTML = data.name + ' - WhatsApp Clone';
 
                 this.el.inputNamePanelEditProfile.innerHTML = data.name;
-                console.log(data);
+                console.log(data.photo);
                 if(data.photo){
 
                     let photo = this.el.imgPanelEditProfile;
@@ -137,6 +139,8 @@ export class WhatsAppController{
                 }
 
                 div.on('click', e => {
+
+                    console.log(contact.chatId);
 
                     this.el.activeName.innerHTML = contact.name;
                     this.el.activeStatus.innerHTML = contact.status;
@@ -330,10 +334,20 @@ export class WhatsAppController{
 
                 if(data.name){
 
-                    this._user.addContact(contact).then(()=>{
+                    Chat.createIfNotExists(this._user.email, contact.email).then(chat => {
 
-                        this.el.btnClosePanelAddContact.click();
-                        console.info('Contato foi adicionado!');
+                        contact.chatId = chat.id;
+
+                        this._user.chatId = chat.id;
+
+                        contact.addContact(this._user);
+
+                        this._user.addContact(contact).then(()=>{
+
+                            this.el.btnClosePanelAddContact.click();
+                            console.info('Contato foi adicionado!');
+    
+                        });
 
                     });
 
