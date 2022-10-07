07a3bf26dcf274726866da92ba37fd8327aa33f1
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-07 10:01:42 -0300

    W35 - Enviando uma mensagem de texto, Created a method to send messages to Firebase

--- projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
+++ projeto-whatsapp-curso-JS/src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ import {DocumentPreviewController} from './DocumentPreviewController';
 import {Firebase} from './../util/Firebase';
 import { User } from '../model/User';
 import { Chat } from '../model/Chat';
+import { Message } from '../model/Message';
 
 
 export class WhatsAppController{
@@ -140,25 +141,7 @@ export class WhatsAppController{
 
                 div.on('click', e => {
 
-                    console.log(contact.chatId);
-
-                    this.el.activeName.innerHTML = contact.name;
-                    this.el.activeStatus.innerHTML = contact.status;
-
-                    if(contact.photo){
-
-                        let img = this.el.activePhoto;
-                        img.src = contact.photo;
-                        img.show();
-
-                    }
-                    
-                    this.el.home.hide();
-                    this.el.main.css({
-
-                        display:'flex'
-
-                    })
+                    this.setActiveChat(contact);
 
                 });
 
@@ -170,6 +153,30 @@ export class WhatsAppController{
         });
 
         this._user.getContacts();
+
+    }
+
+    setActiveChat(contact){
+
+        this._contactActive = contact;
+
+        this.el.activeName.innerHTML = contact.name;
+        this.el.activeStatus.innerHTML = contact.status;
+
+        if(contact.photo){
+
+            let img = this.el.activePhoto;
+            img.src = contact.photo;
+            img.show();
+
+        }
+        
+        this.el.home.hide();
+        this.el.main.css({
+
+            display:'flex'
+
+        })
 
     }
 
@@ -653,7 +660,14 @@ export class WhatsAppController{
 
         this.el.btnSend.on('click', e=>{
 
-            console.log(this.el.inputText.innerHTML);
+            Message.send(
+                this._contactActive.chatId,
+                this._user.email,
+                'text', 
+                this.el.inputText.innerHTML);
+
+            this.el.inputText.innerHTML = '';
+            this.el.panelEmojis.removeClass('open');
 
         });
     }
