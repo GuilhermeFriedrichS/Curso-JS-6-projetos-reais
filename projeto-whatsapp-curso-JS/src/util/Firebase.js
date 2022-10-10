94dc2a756d7b92348c06fc7bca0c3acdbe579cdf
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-10 08:39:44 -0300

    W40 - Enviando imagem da Galeria como anexo, Created method to send image in chat

--- projeto-whatsapp-curso-JS/src/util/Firebase.js
+++ projeto-whatsapp-curso-JS/src/util/Firebase.js
@@ -9,7 +9,7 @@ export class Firebase{
             apiKey: "AIzaSyA0nW_P_7W4TbaPxaGLExE2SDt0z4eQuEU",
             authDomain: "whatsapp-clone-536ee.firebaseapp.com",
             projectId: "whatsapp-clone-536ee",
-            storageBucket: "whatsapp-clone-536ee.appspot.com",
+            storageBucket: "gs://whatsapp-clone-536ee.appspot.com",
             messagingSenderId: "7695932221",
             appId: "1:7695932221:web:a5d0eae88d33ef56968354",
             measurementId: "G-CKH6H7WC1N"
