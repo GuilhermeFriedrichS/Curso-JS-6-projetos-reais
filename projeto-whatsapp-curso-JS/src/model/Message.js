d055cd5acfc30eca1f434a36d6f6d4496042d5f1
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-11 08:25:26 -0300

    W44 - Enviar mensagem para contato anexado, Changed method to send message to contacts sent in chat

--- projeto-whatsapp-curso-JS/src/model/Message.js
+++ projeto-whatsapp-curso-JS/src/model/Message.js
@@ -96,13 +96,6 @@ export class Message extends Model {
                     img.show();
 
                 }
-            
-
-                div.querySelector('.btn-message-send').on('click', e=>{
-
-                    console.log('Enviar mensagem');
-
-                });
 
             break;
 
