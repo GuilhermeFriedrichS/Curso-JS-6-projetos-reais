f3ef5f1aa2dcc71794655e6944b25cf7c2d5fae7
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-19 09:17:17 -0300

    MY36 - Trabalhando com eventos do componente, Created method that receives and executed events

--- projeto-restaurante-curso-JS/saboroso/public/js/hcode-grid.js
+++ projeto-restaurante-curso-JS/saboroso/public/js/hcode-grid.js
@@ -2,6 +2,14 @@ class HcodeGrid {
 
     constructor(configs){
 
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) => {
+                
+                $('#modal-update').modal('show'); 
+                   
+            },
+        }, configs.listeners);
+
         this.options = Object.assign({}, {
             formCreate: '#modal-create form',
             formUpdate: '#modal-update form',
@@ -43,69 +51,77 @@ class HcodeGrid {
 
     }
 
+    fireEvent(name, args){
+
+        if(typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args);
+
+    }
+
     initButtons(){
 
         [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
 
-        btn.addEventListener('click', e=> {
-        
-        let tr = e.path.find(el => {
+            btn.addEventListener('click', e=> {
 
-            return (el.tagName.toUpperCase() === 'TR');
+                this.fireEvent('beforeUpdateClick', [e]);
+            
+                let tr = e.path.find(el => {
 
-        });
+                    return (el.tagName.toUpperCase() === 'TR');
 
-        let data = JSON.parse(tr.dataset.row);
+                });
 
-        for(let name in data){
+                let data = JSON.parse(tr.dataset.row);
 
-            let input = this.formUpdate.querySelector(`[name=${name}]`);
+                for(let name in data){
 
-            switch (name) {
+                    let input = this.formUpdate.querySelector(`[name=${name}]`);
 
-            case 'date':
-            if (input) input.value = moment(data[name]).format('YYYY-MM-DD');
-                break;
+                    switch (name) {
 
-            default:
-                if (input) input.value = data[name];
-            }
+                    case 'date':
+                    if (input) input.value = moment(data[name]).format('YYYY-MM-DD');
+                        break;
 
-        }
+                    default:
+                        if (input) input.value = data[name];
+                    }
 
-        $('#modal-update').modal('show');
-        
-        });
+                }
+               
+                this.fireEvent('afterUpdateClick', [e]);
+                
+            });
 
         });
 
         [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
 
-        btn.addEventListener('click', e=> {
+            btn.addEventListener('click', e=> {
 
-        let tr = e.path.find(el => {
+                let tr = e.path.find(el => {
 
-            return (el.tagName.toUpperCase() === 'TR');
+                    return (el.tagName.toUpperCase() === 'TR');
 
-        });
+                });
 
-        let data = JSON.parse(tr.dataset.row);
+                let data = JSON.parse(tr.dataset.row);
 
-        if(confirm(eval ('`' +this.options.deleteMsg + '`'))){
+                if(confirm(eval ('`' +this.options.deleteMsg + '`'))){
 
-            fetch(eval('`' + this.options.deleteUrl + '`'), {
-            method:'DELETE'
-            })
-            .then(response => response.json())
-            .then(json => {
+                    fetch(eval('`' + this.options.deleteUrl + '`'), {
+                    method:'DELETE'
+                    })
+                    .then(response => response.json())
+                    .then(json => {
 
-                window.location.reload();
+                        window.location.reload();
 
-            })
+                    })
 
-        };
-        
-        });
+                };
+            
+            });
 
         });
 
