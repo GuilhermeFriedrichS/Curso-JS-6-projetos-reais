891388a82b560d345c2b4f4c057521006418eb14
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-19 11:15:12 -0300

    MY39 - Administração dos usuários criando rotas, Created routes and methods to make CRUD of users in MySQL

--- projeto-restaurante-curso-JS/saboroso/routes/admin.js
+++ projeto-restaurante-curso-JS/saboroso/routes/admin.js
@@ -171,7 +171,40 @@ router.delete("/reservations/:id", function(req, res, next){
 
 router.get("/users", function(req, res, next){
 
-    res.render("admin/users", admin.getParams(req));
+    users.getUsers().then(data => {
+
+        res.render("admin/users", admin.getParams(req, {
+            data
+        }));
+
+    });
+
+});
+router.post("/users", function(req, res, next){
+
+    res.save(req.fields).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
+
+});
+router.delete("/users:id", function(req, res, next){
+
+    res.save(req.params.id).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
+
 });
 
 module.exports = router;
